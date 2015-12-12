667118b58893728d129ea6f9b84c98579b88d875
hmqi <1203299890@qq.com>
2015-12-13 04:32:17 +0800

    re

--- wechat/wsgi.py
+++ wechat/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wechat.settings")
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
