bb5b1680d24323079556d8a451f7622b21a64b47
hmqi <1203299890@qq.com>
2015-12-13 03:13:57 +0800

    null

--- wechat/settings.py
+++ wechat/settings.py
@@ -102,7 +102,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-SITE_ROOT = os.path.dirname(os.path.abspath(__file__))
-SITE_ROOT = os.path.abspath(os.path.join(SITE_ROOT, '../'))
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')  #necessary in production!!! union location for all apps'static
+#SITE_ROOT = os.path.dirname(os.path.abspath(__file__))
+#SITE_ROOT = os.path.abspath('../')
+STATIC_ROOT = os.path.join('../', 'static')  #necessary in production!!! union location for all apps'static
+
 STATIC_URL = '/static/'   # set prefix url for static resource.
